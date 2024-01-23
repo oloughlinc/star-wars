01f615a161e6ce4a17d66617d7f8caee0aa7aeb2
oloughlinc <craig.oloughlin@hotmail.com>
2024-01-23 16:50:08 -0500

    basic display to html

--- film.js
+++ film.js
@@ -1,8 +1,8 @@
 const sp = new URLSearchParams(window.location.search);
 const id = sp.get('id');
-const filmsUrl = `https://swapi.azurewebsites.net/api/films/${id}`;
-const charactersUrl = `https://swapi.azurewebsites.net/api/films/${id}/characters`;
-const planetsUrl = `https://swapi.azurewebsites.net/api/films/${id}/planets`;
+const filmsUrl = `https://swapi2.azurewebsites.net/api/films/${id}`;
+const charactersUrl = `https://swapi2.azurewebsites.net/api/films/${id}/characters`;
+const planetsUrl = `https://swapi2.azurewebsites.net/api/films/${id}/planets`;
 
 document.addEventListener('DOMContentLoaded', getFilmDetails)
 
@@ -11,12 +11,16 @@ async function getFilmDetails() {
     // let film = Object;
     // let characters = [];
     // let planets = [];
-    const film = await fetch(filmsUrl, { mode: 'no-cors'}).then()
-    const characters = await fetch(charactersUrl, { mode: 'no-cors'}).then()
-    const planets = await fetch(planetsUrl, { mode: 'no-cors'}).then()
+    const film = await fetch(filmsUrl).then(res => res.json())
+    const characters = await fetch(charactersUrl).then(res => res.json())
+    const planets = await fetch(planetsUrl).then(res => res.json())
     console.log(film)
     console.log(characters)
     console.log(planets)
+
+    document.getElementById('film-info').innerHTML += film.title;
+    document.getElementById('characters').innerHTML += characters.map(item => `<div>${item.name}</div>`)
+    document.getElementById('planets').innerHTML += planets.map(item => `<div>${item.name}</div>`)    
 }
 /*
 planets 
